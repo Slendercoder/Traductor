d11a4420ea856baad0c8c88669bf0e4432a3fcff
Javier Vivas <javiervivas991@gmail.com>
2018-05-03 12:30:57 -0500

    Demo [Ejercicios]

---
+++ demo/lista_palabras.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#Aquí se está una lista de sustantivos 
+np = ["hombre", "mujer", "libro"]
+vi = ["camina", "bebe"]
+vt = ["ama", "invita", "lee"]
+d = ["un", "una", "toda", "todo"]
+c = ["y", "o", "si ... entonces"]
+t = ["Pedro", "Maria", "Juan"]
+
+print(np)
+print(vi)
+print(vt)
+print(d)
+print("©Maria Jose & Javier")
