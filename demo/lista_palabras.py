29a970e33fa9cecb553be9ea426e74356b0a0f49
Javier Vivas <javiervivas991@gmail.com>
2018-05-07 11:54:09 -0500

    Add files via upload

---
+++ demo/lista_palabras.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+#Aquí se está una lista de sustantivos 
+import sys
+print(sys.stdout.encoding)
+np = ["hombre", "mujer", "libro"]
+vi = ["camina", "bebe"]
+vt = ["ama", "invita", "lee"]
+d = ["un", "una", "toda", "todo"]
+c = ["y", "o", "si ... entonces"]
+t = ["Pedro", "Maria", "Juan"]
+
+print(np)
+print(vi)
+print(vt)
+print(d)
+print(u"\u0123")
---
+++ demo/strings.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+words = input("Ingrese su frase: ")
+words2 = words.split(" ")
+print (words2)
+print("©Maria Jose & Javier")
