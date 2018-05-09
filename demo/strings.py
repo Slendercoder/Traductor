db637e48c7bd941415fe037a8402085598b5903f
Slendercoder <andradeed78@gmail.com>
2018-05-09 14:32:27 -0500

    Traduccion NPs y VIs

--- demo/strings.py
+++ demo/strings.py
@@ -1,5 +1,62 @@
 # -*- coding: utf-8 -*-
+
+def infinitivo(verbo):
+    # Toma una cadena que se asume es un verbo
+    # y devuelve una cadena que es el verbo en infinitivo
+    return verbo.upper() + "R"
+
+nc = ["hombre", "mujer", "libro"]
+vi = ["camina", "bebe"]
+vt = ["ama", "invita", "lee"]
+d = ["un", "una", "toda", "todo"]
+c = ["y", "o", "si ... entonces"]
+np = ["pedro", "maria", "juan"]
+
+
 words = raw_input("Ingrese su frase: ")
 words2 = words.split(' ')
-print (words2)
+
+# pasar todas las palabras a minusculas
+# revisar que no queden as
+# Decir que la frase no sirve si hay palabras por fuera del rango
+
+print "La lista de palabras es ", words2
+
+# crear una lista de categorias con base en word2
+categorias = []
+
+for w in words2:
+    if w in np:
+        categorias.append('np')
+    elif w in vi:
+        categorias.append('vi')
+    # faltan las otras categorias
+
+
+print "La lista de categorias es: ", categorias
+
+# crear un diccionario de categorias ???????
+
+traducciones = []
+for i in range(len(words2)):
+    if categorias[i] == 'np':
+        inicial = words2[i][0].lower()
+        traducciones.append(lambda X: X(inicial))
+    elif categorias[i] == 'vi':
+        print "El verbo es: ", words2[i]
+        infini = infinitivo(words2[i])
+        print "El infinitivo es: ", infini
+        traducciones.append(lambda x: str(infini) + "(" + str(x) + ")")
+        # traducciones.append(lambda x: "(" + str(x) + ")")
+
+formula = ''
+
+
+formula = traducciones[0](traducciones[1])
+
+print "La formula en primer orden es: ",
+print formula
+
+
+
 print("©Maria Jose & Javier")
---
+++ traductor.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+#Aquí se está una lista de sustantivos
+np = ["hombre", "mujer", "libro"]
+vi = ["camina", "bebe"]
+vt = ["ama", "invita", "lee"]
+d = ["un", "una", "toda", "todo"]
+c = ["y", "o", "si ... entonces"]
+t = ["Pedro", "Maria", "Juan"]
+
+print(np)
+print(vi)
+print(vt)
+print(d)
+
+words = input("Ingrese su frase: ")
+words2 = words.split(" ")
+print (words2)
+#print("©Maria Jose & Javier")
