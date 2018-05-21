0c54bf7b382f38167b5595b4f480f0b4869dcded
Javier Fernando Vivas Camargo <Programacion-Universidad@Javiers-MacBook-Pro.local>
2018-05-21 12:29:28 -0500

    Quita segundo np (pero falta de las palabras) y revisa si hay determinantes

--- demo/strings.py
+++ demo/strings.py
@@ -6,13 +6,13 @@ def infinitivo(verbo):
     # y devuelve una cadena que es el verbo en infinitivo
     return verbo.upper() + "R"
 
-nc = ["hombre", "mujer", "libro"]
-vi = ["camina", "bebe", "corre"]
-vt = ["ama", "invita", "lee"]
-d = ["un", "una", "toda", "todo"]
-c = ["y", "o", "si ... entonces"]
-np = ["pedro", "maria", "juan", "javier", "jose"]
-
+nc = ["hombre", "mujer", "libro", "animal", "niño","niña","fruta","amigo","amiga","profesor","profesora","edificio","perro", "pajaro","gato","pais","ciudad"]
+vi = ["camina", "bebe", "corre", "duerme","nada","sonrie", "vive", "baila", "come","canta","estornuda","fracasa","grita","llora","patina","pelea","respira","trabaja","viaja"]
+vt = ["ama", "invita", "lee", "compra", "estudia", "escribe","quiere","alimenta","asusta","dice","engaña", "evita", "llama","mira","olvida","necesita","molesta","perdona","visita"]
+d1 = ["un", "una"]
+d2 = ["toda", "todo"]
+c = ["y", "o", "si", "entonces"]
+np = ["pedro", "maria", "juan", "javier", "jose", "angela", "Natalia", "rupaul", "carlos", "fernando", "alejandro", "miguel","esteban", "gabriela","oscar","samuel","victor","edgar", "carlos","eduardo", "sergio", "nicolas", "nathalia","camilo","sebastian","isabella","luisa","sara","rodrigo","angel", "daniel", "manuel","andres","felipe","david", "kevin","cristhian","edwin", "julian"]
 
 words = raw_input("Ingrese su frase: ")
 words1 = words.lower()
@@ -38,8 +38,10 @@ for w in words2:
         categorias.append('vi')
     elif w in vt:
         categorias.append('vt')
-    elif w in d:
-        categorias.append('d')
+    elif w in d1:
+        categorias.append('d1')
+    elif w in d2:
+        categorias.append('d2')
     elif w in c:
         categorias.append('c')
     elif w in np:
@@ -50,7 +52,53 @@ print "La lista de categorias es: ", categorias
 print "La longitud es: ", len(categorias)
 
 # Revisar si hay dos "np"s seguidos y unirlos en uno solo
-# Revisar si hay un "d" y si esta seguido de un "nc"
+indices_para_eliminar = []
+
+for i in range(0, len(categorias) - 1):
+    if categorias[i] == 'np':
+        if categorias[i+1] == 'np':
+            print "Eureka!"
+            indices_para_eliminar.append(i + 1)
+
+
+print indices_para_eliminar
+
+#elimiar indices_para_eliminar de words2 y categorias
+for t in indices_para_eliminar and categorias:
+    if t == 'np':
+        print "Se elimino un ", t
+        categorias.remove(t)
+
+#indices_para_eliminar_nombres = []
+
+#for i in range(0, len(np) - 1):
+#    if np[i] == np[i]:
+#        if np[i+1] == np[i+1]:
+#            print "Yas!"
+#            indices_para_eliminar_nombres.append(i + 1)
+
+
+
+
+#print indices_para_eliminar_nombres
+
+#for t in indices_para_eliminar_nombres and words2:
+#    if t == words2[]:
+#        print "Se elimino un ", t
+#        words2.remove(t)
+
+
+
+print "La lista de palabras es ", words2
+print "La longitud es: ", len(words2)
+
+
+# Revisar si hay un "d" y si esta seguido de un "nc" y avisar si no
+
+for i in range(0, len(categorias) - 1):
+    if categorias[i] == 'd1' or categorias[i] == 'd2':
+        if categorias[i+1] == 'nc':
+            print "Esooooo."
 
 # Decir que la frase no sirve si hay palabras por fuera del rango
 if len(categorias) < len(words2):
@@ -61,9 +109,9 @@ else:
     for i in range(len(words2)):
         if categorias[i] == 'nc':
             print "El sustantivo es: ", words2[i]
-            sust = infinitivo(words2[i])
+            sust = words2[i].lower()
             print "El sustantivo es: ", sust
-            traducciones.append(lambda x: str(sust) + "(" + str(x) + ")")
+            traducciones.append(lambda x, sust=sust: str(sust) + "(" + str(x) + ")")
         elif categorias[i] == 'np':
             inicial = words2[i][0].lower()
             print "La inicial del nombre es: ", inicial
@@ -72,24 +120,30 @@ else:
             print "El verbo es: ", words2[i]
             infini = infinitivo(words2[i])
             print "El infinitivo es: ", infini
-            traducciones.append(lambda x: str(infini) + "(" + str(x) + ")")
+            traducciones.append(lambda x, infini=infini: str(infini) + "(" + str(x) + ")")
         elif categorias[i] == 'vt':
             print "El verbo es: ", words2[i]
             infini = infinitivo(words2[i])
             print "El infinitivo es: ", infini
             traducciones.append(\
-            lambda XX:(lambda x:(XX(lambda y: (str(infini) + "(" + str(x) + "," + str(y) + ")")))))
+            lambda XX:(lambda x:(XX(lambda y, infini=infini: (str(infini) + "(" + str(x) + "," + str(y) + ")")))))
+        elif categorias[i] == 'd1':
+            print "Hay un determinante: ", words2[i]
+            traducciones.append(\
+            lambda X:(lambda Y:(+"∃x (" + X(str(x)) + "∧" + Y(str(x)) + ")")))
+        elif categorias[i] == 'd2':
+            print "Hay un determinante: ", words2[i]
+            traducciones.append(\
+            lambda X:(lambda Y:(+"∀x" + (X(str(x)) + "→" + Y(str(x))))))
 
-    n = len(traducciones)
-    formula = traducciones[n - 1]
-    for i in range(0, n-1):
-        j = (n - 2) - i
-        print "Operando con palabra " + str(j) + "esima"
-        formula = traducciones[j](formula)
+        n = len(traducciones)
+        formula = traducciones[n - 1]
+        for i in range(0, n-1):
+            j = (n - 2) - i
+            print "Operando con palabra " + str(j) + "esima"
+            formula = traducciones[j](formula)
 
-    print "La formula en primer orden es: ",
-    print formula
-
-
+        print "La formula en primer orden es: ",
+        print formula
 
 print("©Maria Jose & Javier")
